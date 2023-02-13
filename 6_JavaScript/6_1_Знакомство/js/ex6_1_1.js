560faea4e864daacfb831820bac1c6f8492d44f2
dv-markov <dmitry.v.markov@gmail.com>
2023-02-14 00:05:59 +0300

    reut

---
+++ 6_JavaScript/6_1_Знакомство/js/ex6_1_1.js
@@ -0,0 +1,14 @@
+let userName = "Alex"
+// alert(userName)
+console.log(userName)
+userName = "Margarett"
+console.log(userName)
+
+let num1 = null;
+console.log(num1);
+num1 = 10;
+console.log(num1);
+
+const COLOR_RED = "#FF0000";
+COLOR_RED = "00FF00";
+console.log(COLOR_RED);
