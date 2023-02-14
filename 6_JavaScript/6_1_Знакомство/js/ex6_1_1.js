df34ac00aee2857b8277a393e923be2309aa0b68
dv-markov <dmitry.v.markov@gmail.com>
2023-02-14 23:48:07 +0300

    reut

--- 6_JavaScript/6_1_Знакомство/js/ex6_1_1.js
+++ 6_JavaScript/6_1_Знакомство/js/ex6_1_1.js
@@ -1,14 +1,22 @@
+console.log("------------------------------------------")
+console.log("Переменные")
+console.log("------------------------------------------")
+
 let userName = "Alex"
 // alert(userName)
 console.log(userName)
 userName = "Margarett"
 console.log(userName)
 
+// при объявлении переменной лучше всегда задавать ей значение
+// если начального значения нет, надо писать null
 let num1 = null;
 console.log(num1);
 num1 = 10;
 console.log(num1);
 
 const COLOR_RED = "#FF0000";
-COLOR_RED = "00FF00";
+// выдаст ошибку
+// COLOR_RED = "00FF00";
 console.log(COLOR_RED);
+
